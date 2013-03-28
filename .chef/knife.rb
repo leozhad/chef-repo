00645d5ca64a15fddc9af4095045220be419b07d
Leo Zhadanovsky <ZhadanovskyL@dnc.org>
2013-03-28 11:55:15 -0400

    Added EC2 key info to knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,3 +9,5 @@ chef_server_url          "https://api.opscode.com/organizations/leo-aws"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
+knife[:aws_access_key_id] = "#{ENV['AWS_ACCESS_KEY_ID']}"
+knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
