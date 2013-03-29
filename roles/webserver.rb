477685df7659defc7fc4b1f9ffa95eb1a5e28306
Leo Zhadanovsky <leozh@amazon.com>
2013-03-29 14:54:00 -0400

    stuff

---
+++ roles/webserver.rb
@@ -0,0 +1,15 @@
+name "webserver"
+description "webserver"
+run_list "recipe[apache]"
+default_attributes({
+    "apache" => {
+      "sites" => {
+        "ponies" => {
+          "port" => 82
+        },
+        "bears" => {
+          "port" => 83
+        }
+      }
+    }
+  })
